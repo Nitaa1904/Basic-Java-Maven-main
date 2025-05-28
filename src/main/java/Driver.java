aeb67881f96eac5334bf546d5931319af05a5828
Nitaa1904 <nitafitrotul1904@gmail.com>
2025-05-28 21:48:41 +0700

    Refactor Driver class to remove duplicate code and unused variables

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,16 +5,27 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		Counter counter = new Counter();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
+
+		// Duplicate code and unused variables
+		int a = 1;
+		int b = 2;
+		int c = a + b;
+
+		System.out.println("Count: " + counter.getCount());
 		counter.increment();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
-		counter.decrement();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
+		System.out.println("Count: " + counter.getCount());
+
+		int x = 1;
+		int y = 2;
+		int z = x + y;
+
+		// Unused method
+		printDebugInfo();
+	}
+
+	// Method never used
+	private static void printDebugInfo() {
+		System.out.println("This is a debug method");
 	}
 }
+
