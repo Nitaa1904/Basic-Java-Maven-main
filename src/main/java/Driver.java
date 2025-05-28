c7c47c95a6ae7a0031b76db1080473796e313451
Nitaa1904 <nitafitrotul1904@gmail.com>
2025-05-28 22:00:54 +0700

    Remove unused variables and methods from Driver class

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -3,29 +3,16 @@ import Counter;
 public class Driver {
 
 	public static void main(String[] args) {
-		
-		Counter counter = new Counter();
 
-		// Duplicate code and unused variables
-		int a = 1;
-		int b = 2;
-		int c = a + b;
+		// NullPointerException potensi
+		String name = null;
+		System.out.println("Length: " + name.length());
 
-		System.out.println("Count: " + counter.getCount());
-		counter.increment();
-		System.out.println("Count: " + counter.getCount());
-
-		int x = 1;
-		int y = 2;
-		int z = x + y;
-
-		// Unused method
-		printDebugInfo();
-	}
-
-	// Method never used
-	private static void printDebugInfo() {
-		System.out.println("This is a debug method");
+		// Infinite loop tanpa break
+		while (true) {
+			System.out.println("This is an infinite loop");
+		}
 	}
 }
+
 
